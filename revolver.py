0cbe387be35948a48a3d7eef63fc1e3fbb2f1bb6
HurrSea <HurrSea@outlook.com>
2020-12-04 17:57:31 +0800

    Add files via upload

--- revolver.py
+++ revolver.py
@@ -4,7 +4,7 @@ from random import randint
 import asyncio
 
 bot = get_bot()
-fd = "./"
+fd = "./dorothy/plugins/rs_data/"  # 存档的绝对路径前缀，记得改成你自己的
 with open(fd + 'rsdata.json') as f:
     data = json.load(f)
 with open(fd + 'rsplayer.json') as f:
@@ -37,21 +37,22 @@ async def spin(session: CommandSession):
 
         if data['curnum'] == 0:
             msg = '欢迎参与紧张刺激的禁言转盘活动，请输入要填入的子弹数目(最多6颗)'
-            bullet = int(session.get('bullet', prompt=msg))
-            if bullet == 6:
+            bullet = session.get('bullet', prompt=msg, at_sender=True)
+            if bullet == '6':
                 ans = session.get('ans', prompt="你认真的？(y/n)")
                 if ans == 'y':
-                    data['curnum'] = bullet
+                    await bot.set_group_ban(group_id=group, user_id=int(user), duration=60)
+                    data['curnum'] = int(bullet)-1
                     data['next'] = 0
                     await session.send("装填完毕")
                 else:
                     session.finish("好吧.")
-            elif bullet < 1 or bullet > 6:
-                session.finish("请输入正确的数目.")
-            else:
-                data['curnum'] = bullet
+            elif '0' < bullet < '6':
+                data['curnum'] = int(bullet)
                 data['next'] = randint(0, 6 - data['curnum'])
                 await session.send("装填完毕")
+            else:
+                session.finish("请输入正确的数目.")
 
         else:
             if data['next'] == 0:
@@ -65,7 +66,7 @@ async def spin(session: CommandSession):
                     await asyncio.sleep(1)
                     msg = ''
                     for k, i in player.items():
-                        msg += ("%s:\nwin: %s \ndeath: %s\n\n" % (i['nickname'], i['win'], i['death']))
+                        msg += ("%s:\nwin: %s death: %s\n" % (i['nickname'], i['win'], i['death']))
                     player.clear()
                     await session.send(msg)
                 else:
@@ -78,3 +79,28 @@ async def spin(session: CommandSession):
 
         save(data, fd + 'rsdata.json')
         save(player, fd + 'rsplayer.json')
+
+
+@spin.args_parser
+async def _(session: CommandSession):
+    if session.is_first_run and session.current_arg_text.strip():
+        # 第一次运行，如果有参数，则设置给 question
+        session.state['bullet'] = session.current_arg_text.strip()
+
+
+@on_command('rd', only_to_me=False)
+async def spin(session: CommandSession):
+    if session.ctx['message_type'] == 'private':
+        msg = '此功能仅适用于群聊'
+        session.finish(msg)
+
+    elif data['curnum'] == 0:
+        session.finish("游戏未开始")
+
+    else:
+        data['curnum'] = 0
+        data['next'] = 0
+        player.clear()
+        save(data, fd + 'rsdata.json')
+        save(player, fd + 'rsplayer.json')
+        session.finish("已取消")
